fffed70fa3af4f9d287ea29a7a5b3e37e7481a05
helmi-ma <masdarhilmi9@gmail.com>
2025-05-02 00:33:50 +0700

    menamabh file

--- tugas/MODUL 2/TUGAS 1/src/Admin.java
+++ tugas/MODUL 2/TUGAS 1/src/Admin.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
+class Admin {
+    private final String username = "HELMI";
+    private final String PW = "149";
 
-public class Admin {
+    public boolean login(String inputUsername, String inputPassword) {
+        return inputUsername.equals(username) && inputPassword.equals(PW);
+    }
 }
--- tugas/MODUL 2/TUGAS 1/src/Mahasiswa.java
+++ tugas/MODUL 2/TUGAS 1/src/Mahasiswa.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+class Mahasiswa {
+    private final String nama = "MASDAR HELMI";
+    private final long nim = 202410370110149L;
 
-public class Mahasiswa {
+    public boolean login(String inputNama, long inputNim) {
+        return inputNama.equals(nama) && inputNim == nim;
+    }
+
+    public void tampilanInfo() {
+        System.out.println("Login Mahasiswa Berhasil!");
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM : " + nim);
+    }
 }
