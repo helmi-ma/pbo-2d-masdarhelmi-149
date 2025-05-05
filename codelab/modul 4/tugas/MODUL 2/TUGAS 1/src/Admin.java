66b6333c7d16d1f6faff55d58d4c07c17722dea8
helmi-ma <masdarhilmi9@gmail.com>
2025-05-05 18:26:13 +0700

    Delete codelab

--- codelab/modul 4/tugas/MODUL 2/TUGAS 1/src/Admin.java
+++
@@ -1,8 +0,0 @@
-class Admin {
-    private final String username = "HELMI";
-    private final String PW = "149";
-
-    public boolean login(String inputUsername, String inputPassword) {
-        return inputUsername.equals(username) && inputPassword.equals(PW);
-    }
-}
